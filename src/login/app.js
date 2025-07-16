14d0705e26c7ced2a1671cfc3f3b4f39b507860c
Agustinsn <agustinasn95@gmail.com>
2025-07-16 12:47:47 -0500

    adding metrics

--- src/login/app.js
+++ src/login/app.js
@@ -3,7 +3,34 @@ import {
     InitiateAuthCommand
 } from "@aws-sdk/client-cognito-identity-provider";
 
+import {
+    CloudWatchClient,
+    PutMetricDataCommand
+  } from "@aws-sdk/client-cloudwatch";
+
 const client = new CognitoIdentityProviderClient({});
+const cloudwatchClient = new CloudWatchClient({});
+
+async function sendMetric({ name, value = 1, unit = "Count", namespace = "Custom/Login" }) {    
+    const command = new PutMetricDataCommand({
+      Namespace: namespace,
+      MetricData: [
+        {
+          MetricName: name,
+          Timestamp: new Date(),
+          Value: value,
+          Unit: unit
+        }
+      ]
+    });
+  
+    try {
+      await cloudwatchClient.send(command);
+      console.log(`Metric sent: ${name} = ${value}`);
+    } catch (err) {
+      console.error("Error sending CloudWatch metric:", err);
+    }
+  }
 
 export async function login(event, context, cognitoClient = null) {
     const cognito = cognitoClient || client;
@@ -12,6 +39,8 @@ export async function login(event, context, cognitoClient = null) {
     const { email, password } = body;
 
     if (!email || !password) {
+        console.warn("Missing email or password");
+        await sendMetric({ name: "LoginFailure_MissingFields" });
         return {
             statusCode: 400,
             body: JSON.stringify({ message: "email & password required" })
@@ -29,6 +58,8 @@ export async function login(event, context, cognitoClient = null) {
 
     try {
         const response = await cognito.send(command);
+        console.log("Login success for user:", email);
+        await sendMetric({ name: "LoginSuccess" });
         return {
             statusCode: 200,
             body: JSON.stringify({
@@ -39,6 +70,8 @@ export async function login(event, context, cognitoClient = null) {
             })
         };
     } catch (err) {
+        console.error("Login failed:", err);
+
         if (err.name === 'NotAuthorizedException') {
             return { statusCode: 401, body: JSON.stringify({ message: "invalid credentials" }) };
         } else if (err.name === 'UserNotConfirmedException') {
@@ -46,6 +79,7 @@ export async function login(event, context, cognitoClient = null) {
         } else if (err.name === 'UserNotFoundException') {
             return { statusCode: 404, body: JSON.stringify({ message: "user not found" }) };
         }
+        await sendMetric({ name: "LoginFailure_UnknownError" });
         return { statusCode: 500, body: JSON.stringify({ error: err.message }) };
     }
 }
